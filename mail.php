d4fec858253d114c6e2502c7b9eeb211818f478a
reanimatory <42819086+reanimatory@users.noreply.github.com>
2019-08-14 10:58:45 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,11 +1,41 @@
- <?php
-$to = "reanimatory1990@gmail.com"; // емайл получателя данных из формы 
-$tema = "Форма обратной связи на PHP"; // тема полученного емайла 
-$message = "Ваше имя: ".$_POST['name']."<br>";//присвоить переменной значение, полученное из формы name=name
-  $message .= "E-mail: ".$_POST['email']."<br>"; //полученное из формы name=email
-$message .= "Номер телефона: ".$_POST['phone']."<br>"; //полученное из формы name=phone
-$message .= "Сообщение: ".$_POST['message']."<br>"; //полученное из формы name=message
-$headers  = 'MIME-Version: 1.0' . "\r\n"; // заголовок соответствует формату плюс символ перевода строки
-  $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; // указывает на тип посылаемого контента
-mail($to, $tema, $message, $headers); //отправляет получателю на емайл значения переменных
+<?php
+
+/* Задаем переменные */
+$name = htmlspecialchars($_POST["name"]);
+$email = htmlspecialchars($_POST["email"]);
+$tel = htmlspecialchars($_POST["tel"]);
+$website = htmlspecialchars($_POST["website"]);
+$message = htmlspecialchars($_POST["message"]);
+$bezspama = htmlspecialchars($_POST["bezspama"]);
+
+/* Ваш адрес и тема сообщения */
+$address = "reanimatory1990@gmail.com";
+$sub = "Сообщение с сайта ХХХ";
+
+/* Формат письма */
+$mes = "Сообщение с сайта ХХХ.\n
+Имя отправителя: $name 
+Электронный адрес отправителя: $email
+Телефон отправителя: $tel
+Сайт отправителя: $website
+Текст сообщения:
+$message";
+
+
+if (empty($bezspama)) /* Оценка поля bezspama - должно быть пустым*/
+{
+/* Отправляем сообщение, используя mail() функцию */
+$from  = "From: $name <$email> \r\n Reply-To: $email \r\n";
+if (mail($address, $sub, $mes, $from)) {
+    header('Refresh: 5; URL=https://biznessystem.ru');
+    echo '
+    
+    Письмо отправлено, через 5 секунд вы вернетесь на страницу XXX%MINIFYHTML54fe9198634ad8ae8f9ea710a02e43a210%';}
+else {
+    header('Refresh: 5; URL=https://biznessystem.ru');
+    echo '
+    
+    Письмо не отправлено, через 5 секунд вы вернетесь на страницу YYY';}
+}
+exit; /* Выход без сообщения, если поле bezspama заполнено спам ботами */
 ?>
